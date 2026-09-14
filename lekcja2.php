21307b64b6682fe1597506470439f086a68d9cf0
zebrogamesyt <michal.zebrowski45@gmail.com>
2026-09-14 14:13:32 +0200

    Add files via upload

---
+++ lekcja2.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 27;
+echo $a . "
+";
+echo -$a . "
+",
+echo $a + 3 . "
+";
+echo $a - 5 . "
+";
+echo $a * 2 . "
+";
+echo $a / 5 . "
+";
+echo $a % 2 . "
+";
+echo $a ** 3 . "
+";
+echo "Wartość zmiennej \$a wynosi: $a i już się nie zmieni.";
+
+?>
